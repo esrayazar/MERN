f305eba9968353b10de2ed35b55b1afeebb07110
Esra Yazar <esrayzr@gmail.com>
2021-09-28 22:26:51 -0400

    adding new files

--- JavaScript/assignment/predictDestructuring.js
+++
@@ -1,7 +0,0 @@
-const cars = ['Tesla', 'Mercedes', 'Honda']
-const [ randomCar ] = cars
-const [ ,otherRandomCar ] = cars
-//Predict the output
-console.log(randomCar)
-console.log(otherRandomCar)
-
