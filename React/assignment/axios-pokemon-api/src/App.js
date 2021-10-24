347ee190bab60220164acf1d261fd5e2bb771301
Esra Yazar <esrayzr@gmail.com>
2021-10-23 21:12:39 -0400

    pokemon axios done

---
+++ React/assignment/axios-pokemon-api/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Homepage from './components/Homepage';
+import React from 'react'
+
+
+function App() {
+  
+  return (
+    <div className="App">
+      <Homepage/>
+  
+    </div>
+  );
+}
+
+export default App;
